08d475c41a649f2074c964a1bef2d76a95795168
Mateus <maatmds04@gmail.com>
2023-04-18 14:35:18 -0300

    first commit

---
+++ src/main/java/com/gamedown/gamedown/GamedownApplication.java
@@ -0,0 +1,13 @@
+package com.gamedown.gamedown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedownApplication.class, args);
+	}
+
+}
